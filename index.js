50682560e914ccd055b2128af947aca8e7e0afb2
Kristina <kristin.petrivska@gmail.com>
2024-07-12 15:33:24 -0600

    add js code with prompt and alert

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-
+'use strict';
+const userName = prompt("What is your first name?");
+const userNametrimed = userName.trim();
+alert(userNametrimed ? `Hello, ${userNametrimed}. How are you?` : `Hello stranger! How are you?` )
