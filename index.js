6668ee4ede8c0f9bf87a2d3681085e867f67911b
Kristina <kristin.petrivska@gmail.com>
2024-07-14 14:25:31 -0600

    add validation for cancel button

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 'use strict';
 const userName = prompt("What is your first name?");
+if (userName === null){
+    alert('You canceled the input')
+}else{
 const userNametrimed = userName.trim();
 alert(userNametrimed ? `Hello, ${userNametrimed}. How are you?` : `Hello stranger! How are you?` )
+}
